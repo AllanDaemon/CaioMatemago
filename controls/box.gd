ead2683e4dbb4c907a2eff3282080566351b4db9
Allan Daemon <rea.aft@gmail.com>
2017-11-02 06:37:56 -0200

    stucked on different sprite for differents boxes

--- controls/box.gd
+++ controls/box.gd
@@ -1,22 +1,54 @@
 extends KinematicBody2D
 
 onready var sprite = get_node("sprite")
+onready var anim = get_node("anim")
 
 signal value_change
 
 export (int) var increment = 1
-export (Texture) var _texture
-export (Rect2) var _region
+export (Texture) var texture
+export (Rect2) var region_default
+export (Rect2) var region_hit
 
 func _ready():
-	if _texture and _region:
-		sprite.set_texture(_texture)
-		sprite.set_region_rect(_region)
+	print("Box ", self.get_name())
+	print("\ttexture: ", texture)
+	print("\tregion_default: ", region_default)
+	print("\tregion_hit: ", region_hit)
+
+	if texture and texture != null:
+		sprite.set_texture(texture)
+		print("\ttexture setted")
+	if region_default != null:
+		sprite.set_region_rect(region_default)
+		print("\tdefault region setted")
+		
+		var hit = anim.get_animation("hit")
+		hit.track_set_key_value(0, 1, region_default)
+		
+	if region_hit != null:
+		var hit = anim.get_animation("hit")
+		hit.track_set_key_value(0, 0, region_hit)
+		print("\thitregion setted")
 
 
 func _on_area_body_enter( body ):
 	if body.is_in_group("player"):
 		emit_signal("value_change", increment)
 		get_node("anim").play("hit")
-	print(body)
-	
+		print(body)
+
+
+func _xp():
+	var anim = get_node("anim")
+	var res = anim.get_animation_list()
+	print ("res: ", res)
+	var hit = anim.get_animation("hit")
+	print ("res: ", hit)
+	var res = hit.track_get_path(0)
+	print ("res: ", res)
+	var res = hit.track_get_key_value(0, 0)
+	print ("res: ", res)
+	var res = hit.track_get_key_value(0, 1)
+	print ("res: ", res)
+	hit.track_set_key_value(0, 0, res)
