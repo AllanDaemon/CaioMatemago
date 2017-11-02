16dc0b65d624a7ac6adae0b4213613a7245a2548
Allan Daemon <rea.aft@gmail.com>
2017-11-01 22:28:10 -0200

    implement player movements

--- player/player.gd
+++ player/player.gd
@@ -1,18 +1,60 @@
 extends KinematicBody2D
 
-const ACCEL = 1500
-const MAX_VEL = 500
-const FRICTION = 500
+# Based on
+# https://github.com/kidscancode/godot_tutorials/blob/master/Godot101/Part%2012/player.gd
+
+
+onready var ground_ray = get_node("ground_ray")
+onready var sprite = get_node("sprite")
+onready var animation = get_node("anim")
+
+const ACCEL = 1200
+const MAX_SPEED = 100
+const FRICTION = -500
+const GRAVITY = 1000
+const JUMP_SPEED = -450
+const MIN_JUMP = -100
 
 var acc = Vector2()
 var vel = Vector2()
+var anim = "idle"
 
 func _ready():
 	set_fixed_process(true)
-	
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("ui_up") and ground_ray.is_colliding():
+		vel.y = JUMP_SPEED
+	if event.is_action_released("ui_up"):
+		vel.y = clamp(vel.y, MIN_JUMP, vel.y)
+
 func _fixed_process(delta):
-	acc.x = Input.is_action_pressed("move_right") - Input.is_action_pressed("move_left")
-	acc *= ACCEL
-	vel.x += acc.x * delta
-	vel.x = clamp(vel.x, -MAX_VEL, MAX_VEL)
-	move(vel * delta)
+	acc.y = GRAVITY
+	acc.x = Input.is_action_pressed("ui_right") - Input.is_action_pressed("ui_left")
+	acc.x *= ACCEL
+	if acc.x == 0:
+		acc.x = vel.x * FRICTION * delta
+
+	vel += acc * delta
+	vel.x = clamp(vel.x, -MAX_SPEED, MAX_SPEED)
+
+	var motion = move(vel * delta)
+	if is_colliding():
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		vel = n.slide(vel)
+		move(motion)
+	if abs(vel.x) < 20:
+		vel.x = 0
+
+	# set animation
+	if vel.x == 0:
+		anim = "idle"
+	else:
+		anim = "running"
+	if vel.x > 0:
+		sprite.set_flip_h(false)
+	elif vel.x < 0:
+		sprite.set_flip_h(true)
+	animation.play(anim)
