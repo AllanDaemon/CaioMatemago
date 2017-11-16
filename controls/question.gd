bff1ec13233346988308fbda25a24e7758913aaa
Allan Daemon <rea.aft@gmail.com>
2017-11-16 07:19:26 -0200

    create question widget

---
+++ controls/question.gd
@@ -0,0 +1,15 @@
+extends Control
+
+export (String) var question = "1 + 2" setget _set_question
+export (int) var result = 3
+
+onready var label = get_node("Patch9Frame/Label")
+
+func _ready():
+	_set_question(question)
+	
+func _set_question(value):
+	question = value
+	if label:
+		print("VALUE SET: ", value)
+		label.set_text(value)
