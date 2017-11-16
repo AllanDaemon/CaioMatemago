497137ff4dbb6a72380001db50890b980a1698bc
Allan Daemon <rea.aft@gmail.com>
2017-11-16 07:20:44 -0200

    update tileset

--- game.gd
+++ game.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
+signal right
+signal wrong
+
 onready var boxes = get_node("boxes")
+onready var question = get_node("question")
 
 var result = 0
-
 
 func _ready():
 	print("Boxes: ", boxes)
 	for box in boxes.get_children():
 		print("box: ", box)
 		box.connect("value_change", self, "_on_value_change")
+		box.connect("result", self, "_on_result")
 	pass
 
 func _on_value_change(value):
@@ -19,3 +23,15 @@ func _on_value_change(value):
 	
 func _update_label():
 	get_node("result").set_text(str(result))
+	
+func _on_result():
+	if result == question.result:
+		emit_signal("right")
+	else:
+		emit_signal("wrong")
+
+func right():
+	pass
+	
+func wrong():
+	pass
