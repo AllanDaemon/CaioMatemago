efa259d22d9b12ebdcfd225388b5f716ddd46b29
Allan Daemon <rea.aft@gmail.com>
2018-01-25 07:23:29 -0200

    enemy s/kinematic/rigbd/

--- player/enemy.gd
+++ player/enemy.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 export (int) var speed = 100
 export (Vector2) var movement = Vector2(1, 1)	# Pixel / secs
@@ -7,7 +7,8 @@ export (bool) var enable_on_tool = false
 onready var sprite = get_node("sprite_anim")
 
 func _ready():
-	set_fixed_process(true)
+#	set_fixed_process(true)
+	pass
 
 func _fixed_process(delta):
 	var remain = move(movement * speed * delta)
@@ -15,4 +16,6 @@ func _fixed_process(delta):
 		movement.x *= -1
 		sprite.set_flip_h( movement.x > 0)
 
+#func _integrate_forces(state):
+#	pass
 	
