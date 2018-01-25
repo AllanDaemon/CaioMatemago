75b8681c3ea60664f2eb30207e037466526f2240
Allan Daemon <rea.aft@gmail.com>
2018-01-25 06:49:28 -0200

    enemy control

--- player/enemy.gd
+++ player/enemy.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-export (Vector2) var movement = Vector2(1, 0)	# Pixel / secs
+export (int) var speed = 100
+export (Vector2) var movement = Vector2(1, 1)	# Pixel / secs
 export (bool) var enable_on_tool = false
 
 onready var sprite = get_node("sprite_anim")
@@ -9,9 +10,9 @@ func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	var remain = move(movement * delta)
-	if remain:
-		movement *= -1
+	var remain = move(movement * speed * delta)
+	if remain.x:
+		movement.x *= -1
 		sprite.set_flip_h( movement.x > 0)
 
 	
