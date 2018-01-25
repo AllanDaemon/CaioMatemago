806abe0d7742aafae2aa70c637ddb4f19b5ade15
Allan Daemon <rea.aft@gmail.com>
2018-01-25 17:19:30 -0200

    question org.

--- scenario/question.gd
+++ scenario/question.gd
@@ -6,7 +6,7 @@ signal wrong
 export (String) var question = "1 + 2" setget _set_question
 export (int) var result = 3
 
-onready var label = get_node("Patch9Frame/Label")
+onready var label = get_node("frame/result_label")
 
 func _ready():
 	_set_question(question)
