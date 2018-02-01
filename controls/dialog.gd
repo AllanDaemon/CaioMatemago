53848dff659d390b7b413862d16046996e545d67
Allan Daemon <rea.aft@gmail.com>
2018-02-01 04:01:36 -0200

    dialog show script

--- controls/dialog.gd
+++ controls/dialog.gd
@@ -1,3 +1,4 @@
+tool
 extends Control
 
 var character = "Prof. Candango"
@@ -11,5 +12,32 @@ var lines_alt = [
 	["não se esqueça de saltar obstáculos", "e inimigos se quiser sobreviver."],
 ]
 
+var curr_line = lines[1]
+
+onready var char_label = get_node("char_label")
+onready var text_label = get_node("frame/text_label")
+
 func _ready():
-	pass
+	_update()
+
+func _update():
+	_update_char()
+	_update_text()
+	
+func _update_char():
+	char_label.set_text(character)
+
+func _update_text(play_anim=false):
+	text_label.set_text(curr_line)
+	get_node("anim").play("show_line")
+
+func show_text(line, char=null):
+	if char:
+		prints("Update char")
+		character = char
+		_update_char()
+	else:
+		prints("No update char")
+
+	curr_line = line
+	_update_text(true)
