4e24b4c82b8a282b02fa6489b6b5113cae686053
Allan Daemon <rea.aft@gmail.com>
2018-02-01 07:36:21 -0200

    add all lines from screenplay.

--- player/lines.gd
+++ player/lines.gd
@@ -6,12 +6,42 @@ const candangoA1a = [
 ]
 
 const candangoA1b = [
-"Procurando uma saída?",
-"Essa porta não abrirá enquanto \nesses monstros estiverem vivos.",
-"Mas daqui eu não consigo atingi-los!"
+	"Procurando uma saída?",
+	"Essa porta não abrirá enquanto \nesses monstros estiverem vivos.",
+	"Mas daqui eu não consigo atingi-los!"
 ]
 
+const candangoA1c = [
+	"Uau! Você consegue derrotá-los!",
+	"Faça isso mais duas vezes \npara podermos seguir em frente."
+]
+
+const candangoA2a = [
+	"Estes blocos devem servir para mais \ncoisas do que só afugentar nossos inimigos.",
+	"A ponte está desativada, será que ela \npode ser reativada se resolvermos essa conta?"
+]
+
+const candangoA4a = [
+	"Moedas! Muitas moedas! Você pode me \najudar a juntar esses trocados?",
+	"Quantas moedas foram deixadas aqui?"
+]
+
+
+const bossA5a = [
+	"Você resolveu 5 contas para chegar ao meu castelo? Dúvido que seja verdade! Eu sou o rei do vale da soma e só eu sou capaz de controlar a matemática! Eu te desafio a resolver ... [delay] 10 contas!!!"
+]
+
+const candangoA5a = [
+	"Parabéns! Você derrotou o Rei Belingerante. Agora o vale da soma não está mais na mão desses monstros horríveis. O próximo passo da sua jornada é a montanha da subtração. Ouvi falar que as contas de lá são mais difíceis, mas acho que é só um boato para assustar crianças. Tenho certeza que você triunfará mais uma vez! Boa sorte."
+]
+
+# Uncomment if the line has been tested and adjusted
 const lines_map = {
 	"candangoA1a": candangoA1a,
-	"candangoA1b": candangoA1b
+	"candangoA1b": candangoA1b,
+	#"candangoA1c": candangoA1c,
+	#"candangoA2a": candangoA2a,
+	#"candangoA4a": candangoA4a,
+	#"bossA5a": bossA5a,
+	#"candangoA5a": candangoA5a,
 }
