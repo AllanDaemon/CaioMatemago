889f333650e7564943787a30aca20682be43e1bd
Allan Daemon <rea.aft@gmail.com>
2018-02-01 02:51:45 -0200

    WIP add dialog for dialog box

---
+++ controls/dialog.gd
@@ -0,0 +1,15 @@
+extends Control
+
+var character = "Prof. Candango"
+var lines = [
+	"Ei, garoto! Cuidado com o buraco.\nO vale da soma é um lugar perigoso,",
+	"não se esqueça de saltar obstáculos\ne inimigos se quiser sobreviver.",
+]
+
+var lines_alt = [
+	["Ei, garoto! Cuidado com o buraco.", "O vale da soma é um lugar perigoso,"],
+	["não se esqueça de saltar obstáculos", "e inimigos se quiser sobreviver."],
+]
+
+func _ready():
+	pass
