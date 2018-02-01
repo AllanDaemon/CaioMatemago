d9d6c938b68c6c8fd05216b1b01da5856cd01db5
Allan Daemon <rea.aft@gmail.com>
2018-02-01 04:21:28 -0200

    WIP Test with dialog on NPC

--- controls/dialog.gd
+++ controls/dialog.gd
@@ -8,17 +8,23 @@ var lines = [
 ]
 
 var lines_alt = [
-	["Ei, garoto! Cuidado com o buraco.", "O vale da soma é um lugar perigoso,"],
-	["não se esqueça de saltar obstáculos", "e inimigos se quiser sobreviver."],
+	["Ei, garoto! Cuidado com o buraco.",
+	"O vale da soma é um lugar perigoso,"],
+	["não se esqueça de saltar obstáculos",
+	"e inimigos se quiser sobreviver."],
 ]
 
 var curr_line = lines[1]
 
 onready var char_label = get_node("char_label")
 onready var text_label = get_node("frame/text_label")
+onready var anim = get_node("anim")
+
 
 func _ready():
-	_update()
+#	_update()
+#	show_text(lines)
+	pass
 
 func _update():
 	_update_char()
@@ -31,13 +37,16 @@ func _update_text(play_anim=false):
 	text_label.set_text(curr_line)
 	get_node("anim").play("show_line")
 
-func show_text(line, char=null):
+func show_line(line, char=null):
 	if char:
-		prints("Update char")
 		character = char
 		_update_char()
-	else:
-		prints("No update char")
 
 	curr_line = line
 	_update_text(true)
+
+func show_text(_lines, char=null):
+	lines = _lines
+	for line in lines:
+		show_line(line, char)
+		yield(anim, "finished")
--- player/candango.gd
+++ player/candango.gd
@@ -1,7 +1,16 @@
 extends Area2D
 
+signal npc_action
+
+onready var dialog = get_node("../dialog")
+
 func _ready():
-	pass
+	connect("npc_action", self, "_on_npc_action")
 
 func _on_player_near(body):
 	printt("Player near.", body)
+	emit_signal("npc_action")
+	
+func _on_npc_action():
+	dialog.show()
+	dialog.show_text(dialog.lines)
