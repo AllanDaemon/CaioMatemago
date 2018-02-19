0ed655a225c04393111b194767e732f9b1626878
Allan Daemon <rea.aft@gmail.com>
2018-02-19 20:20:09 -0300

    prepre to bubble boxes

--- levels/A2.gd
+++ levels/A2.gd
@@ -1,4 +1,19 @@
 extends Node2D
 
 onready var common = get_node("common")
+onready var boxp1 = common.get_node("boxes/box+1")
+onready var boxm1 = common.get_node("boxes/box-1")
 
+const bblock_max = 7
+var bblock_count = 0
+
+func _ready():
+	boxp1.connect("value_change", self, "_on_value_change")
+	boxm1.connect("value_change", self, "_on_value_change")
+	
+func _on_value_change(value):
+	bblock_count += value
+	update_blocks()
+
+func update_blocks():
+	printt("Bubble block count", bblock_count)
