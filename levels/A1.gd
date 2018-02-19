2697ed2c8369979b5ea9261ecaefc0138e2e8911
Allan Daemon <rea.aft@gmail.com>
2018-02-19 16:38:52 -0300

    A1 Level

--- levels/A1.gd
+++ levels/A1.gd
@@ -1,3 +1,21 @@
 extends Node2D
 
 onready var common = get_node("common")
+onready var questions = common.get_node("questions")
+onready var door = common.get_node("math/door")
+
+func _ready():
+	for q in questions.get_children():
+		q.connect("right_operation", self, "_on_wright_operation")
+
+func _on_wright_operation():
+	for q in questions.get_children():
+		print("RESULT:")
+		if q.status != q.RIGHT:
+			prints("\tWRONG")
+			return
+	prints("ALL WRIGHT")
+	level_done()
+	
+func level_done():
+	door.open = true
