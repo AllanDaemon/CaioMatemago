009877cf7fb15cd5292ebd6213bdc86f997985cb
Allan Daemon <rea.aft@gmail.com>
2018-02-20 05:44:53 -0300

    A5 interactions

--- enemies/boss.gd
+++ enemies/boss.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal interaction_end
+
 onready var dialog = get_node("dialog")
 onready var lines_load = get_node("/root/lines")
 onready var line = lines_load.bossA5a
@@ -10,3 +12,8 @@ func _ready():
 func first_interaction():
 	dialog.show()
 	dialog.show_text(line, "Rei Belingerante")
+	print("Before yield")
+	yield(dialog, "lines_end")
+	print("After yield")
+	emit_signal("interaction_end")
+	return
--- levels/A5.gd
+++ levels/A5.gd
@@ -18,11 +18,18 @@ const questions = [
 ]
 
 onready var common = get_node("common")
+onready var anim = get_node("anim")
 onready var question = common.get_node("questions/question_group")
 onready var question_timer = question.get_node("timer")
+onready var boss = common.get_node("enemies/boss")
 
 func _ready():
+	boss.first_interaction()
+	print("waiting boss interaction")
+	yield(boss, "interaction_end")
+	print("going next interaction")
 	_setup_next_question()
+	anim.play("begin_operations")
 
 func _on_right_operation():
 	print("VI PRAVAS")
@@ -44,3 +51,4 @@ func _setup_next_question():
 func _boss_defeat():
 	printt("Boss defeated!", operations_done)
 	boss_defeated = true
+	anim.play("boss_defeated")
