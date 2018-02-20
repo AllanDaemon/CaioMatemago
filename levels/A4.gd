35fea7c61841acef079999596556bef4b90cd7fa
Allan Daemon <rea.aft@gmail.com>
2018-02-20 01:15:25 -0300

    A4 coin as +1

--- levels/A4.gd
+++ levels/A4.gd
@@ -1,16 +1,32 @@
 extends Node2D
 
+const coins_total = 40
+
 onready var common = get_node("common")
-onready var question1 = common.get_node("questions/question_group1")
+onready var coins = common.get_node("coins")
+onready var display = common.get_node("questions/question1_display")
+#onready var question1 = common.get_node("questions/question_group1")
 onready var question2 = common.get_node("questions/question_group2")
 onready var wall = common.get_node("blocks/wall")
 onready var bridge = common.get_node("blocks/bridge")
+onready var door = common.get_node("math/door")
 
 func _ready():
-	question1.connect("right_operation", wall, "queue_free")
+	_setup_coins()
 	question2.connect("right_operation", self, "activate_bridge")
+	question2.connect("right_operation", door, "open")
 
 func activate_bridge():
 	printt("Activating bridge")
 	for block in bridge.get_children():
 		block.enabled = true
+
+func _setup_coins():
+	assert(coins.get_child_count() == coins_total)
+	for coin in coins.get_children():
+		coin.connect("coin", self, "coin_up")
+
+func coin_up(up=1):
+	display.value += up
+	if display.value == coins_total:
+		display.right()
