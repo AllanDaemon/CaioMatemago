4b45673da5002d0e8d14fb22dfd3efc341a81345
Allan Daemon <rea.aft@gmail.com>
2018-02-19 23:13:07 -0300

    buuble box hide on disabled

--- scenario/bubble_box.gd
+++ scenario/bubble_box.gd
@@ -3,18 +3,20 @@ extends KinematicBody2D
 
 export (bool) var enabled = false setget _set_enabled
 
-onready var sprite_on = get_node("sprite_enabled")
-onready var sprite_off = get_node("sprite_disabled")
+#onready var sprite_on = get_node("sprite_enabled")
+#onready var sprite_off = get_node("sprite_disabled")
+onready var sprite = get_node("sprite")
 onready var anim = get_node("anim")
 onready var _post_ready = true
 
 func _get_item_rect():
-    return get_node("sprite_enabled").get_item_rect()
+    return sprite.get_item_rect()
 
 func _ready():
 	_set_enabled(enabled)
 
 func _set_enabled(value):
+	if not get_tree() or get_tree().is_editor_hint(): return
 	enabled = value
 	if not _post_ready: return
 	anim.play("enabled" if enabled else "disabled")
