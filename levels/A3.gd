81cfe92bf383f6f5f2d77738842b651e299d5bc5
Allan Daemon <rea.aft@gmail.com>
2018-02-20 00:03:39 -0300

    A3 first wall

--- levels/A3.gd
+++ levels/A3.gd
@@ -1,4 +1,8 @@
 extends Node2D
 
 onready var common = get_node("common")
+onready var first_wall = common.get_node("blocks/first_wall")
+onready var question1 = common.get_node("questions/question_group1")
 
+func _ready():
+	question1.connect("right_operation", first_wall, "queue_free")
