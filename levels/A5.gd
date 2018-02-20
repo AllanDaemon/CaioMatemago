8a63b927de68e91bc7cb56fa4380fcc74152daeb
Allan Daemon <rea.aft@gmail.com>
2018-02-20 02:29:16 -0300

    A5 progress

--- levels/A5.gd
+++ levels/A5.gd
@@ -1,4 +1,46 @@
 extends Node2D
 
-onready var common = get_node("common")
+#export (int, 0, 100) var operations_max = 10
+var operations_done = 0
+var boss_defeated = false
 
+const questions = [
+	[1, 0],		# 01
+	[1, 1],		# 02
+#	[1, 2],		# 03
+#	[1, 3],		# 04
+#	[1, 4],		# 05
+	[0, 0],		# 06
+	[0, 1],		# 07
+#	[0, 2],		# 08
+#	[0, 3],		# 09
+#	[0, 4],		# 10
+]
+
+onready var common = get_node("common")
+onready var question = common.get_node("questions/question_group")
+onready var question_timer = question.get_node("timer")
+
+func _ready():
+	_setup_next_question()
+
+func _on_right_operation():
+	print("VI PRAVAS")
+	operations_done += 1
+	question_timer.start()
+	yield (question_timer, "timeout")
+	if questions.empty():
+		_boss_defeat()
+	else:
+		_setup_next_question()
+	
+func _setup_next_question():
+	var next = questions[0]
+	questions.pop_front()
+	question.a = next[0]
+	question.b = next[1]
+	question.update_question()
+
+func _boss_defeat():
+	printt("Boss defeated!", operations_done)
+	boss_defeated = true
