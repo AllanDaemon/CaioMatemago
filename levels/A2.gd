615724442dca085a306bff5e8a750cfd42c675ee
Allan Daemon <rea.aft@gmail.com>
2018-02-19 21:45:26 -0300

    A2: user question group

--- levels/A2.gd
+++ levels/A2.gd
@@ -1,26 +1,13 @@
 extends Node2D
 
 onready var common = get_node("common")
-onready var boxp1 = common.get_node("boxes/box+1")
-onready var boxm1 = common.get_node("boxes/box-1")
 onready var bblocks = common.get_node("math/bubble_blocks")
-onready var value_display = common.get_node("math/value_display")
-
-const bblock_max = 7
-var bblock_count = 0
+onready var question1 = common.get_node("questions/question_group")
 
 func _ready():
-	boxp1.connect("value_change", self, "_on_value_change")
-	boxm1.connect("value_change", self, "_on_value_change")
-	_on_value_change(0)
+	question1.connect("right_operation", self, "activate_bridge")
 	
-func _on_value_change(value):
-	bblock_count += value
-	value_display.value = bblock_count
-	if bblock_count == bblock_max:
-		activate_bridge()
-
 func activate_bridge():
-	printt("Activating bridge", bblock_count)
+	printt("Activating bridge")
 	for block in bblocks.get_children():
 		block.enabled = true
