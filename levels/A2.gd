9db44e7d8177f022ac5154e84bcfc84fcd7941d0
Allan Daemon <rea.aft@gmail.com>
2018-02-19 21:29:23 -0300

    level A2 progress

--- levels/A2.gd
+++ levels/A2.gd
@@ -3,6 +3,8 @@ extends Node2D
 onready var common = get_node("common")
 onready var boxp1 = common.get_node("boxes/box+1")
 onready var boxm1 = common.get_node("boxes/box-1")
+onready var bblocks = common.get_node("math/bubble_blocks")
+onready var value_display = common.get_node("math/value_display")
 
 const bblock_max = 7
 var bblock_count = 0
@@ -10,10 +12,15 @@ var bblock_count = 0
 func _ready():
 	boxp1.connect("value_change", self, "_on_value_change")
 	boxm1.connect("value_change", self, "_on_value_change")
+	_on_value_change(0)
 	
 func _on_value_change(value):
 	bblock_count += value
-	update_blocks()
+	value_display.value = bblock_count
+	if bblock_count == bblock_max:
+		activate_bridge()
 
-func update_blocks():
-	printt("Bubble block count", bblock_count)
+func activate_bridge():
+	printt("Activating bridge", bblock_count)
+	for block in bblocks.get_children():
+		block.enabled = true
