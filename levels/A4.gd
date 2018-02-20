43977133e26ccfae10c24e1129cc373130ab0716
Allan Daemon <rea.aft@gmail.com>
2018-02-20 00:53:58 -0300

    A4 question 2

--- levels/A4.gd
+++ levels/A4.gd
@@ -1,4 +1,16 @@
 extends Node2D
 
 onready var common = get_node("common")
+onready var question1 = common.get_node("questions/question_group1")
+onready var question2 = common.get_node("questions/question_group2")
+onready var wall = common.get_node("blocks/wall")
+onready var bridge = common.get_node("blocks/bridge")
 
+func _ready():
+	question1.connect("right_operation", wall, "queue_free")
+	question2.connect("right_operation", self, "activate_bridge")
+
+func activate_bridge():
+	printt("Activating bridge")
+	for block in bridge.get_children():
+		block.enabled = true
